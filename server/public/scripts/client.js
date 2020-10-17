b3485cbdaf69f9a6ea4b1b242f33725b0ce89bec
Matthew Harris <matthewharrisak@gmail.com>
2020-10-17 15:59:30 -0500

    set up post request / things aren't appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,43 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('hello from JQ');
+    $('#submitNewTask').on('click' , newTaskSubmission)
 }
 
+
+// post request that receives input values from DOM and sends to server
+function newTaskSubmission () {
+    console.log('whats up from newTaskSubmission');
+    let newTaskObject = {
+        task: $('#taskInput').val(),
+        dueBy:$('#dueDateInput').val()
+    }
+    $.ajax({
+        type:'POST',
+        url: './taskRoutes',
+        data: newTaskObject
+         }).then(function(response){
+            $('#taskInput').val(''),
+             $('#dueDateInput').val(''),
+             taskHistory();
+                });
+}
+
+// get request to get data from database
+function taskHistory () {
+    $('#taskRowBody').empty(); // emptys out the DOM before the same data is reloaded
+    $.ajax({
+        type: 'GET',
+        url: '/taskRoutes'
+     }).then(function(response){
+             console.log('any reponse?!' , response);
+             for (let index = 0; index < response.length; index++) {
+              $('#taskRowBody').append(`
+              <tr>
+                 <td>${response[index].task}</td>
+                 <td>${response[index].dueBy}</td>
+                </tr>
+              `);   
+             }
+      });
+}
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -2,6 +2,28 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
+
+
+router.post('/' , (req, res) => { // sending the stuff
+    console.log('req.body' , req.body);
+    let task = req.body.task;
+    let dueBy = req.body.dueBy;
+    
+    let queryText = `INSERT INTO "tasks" ("task" , "dueBy")
+    VALUES('${task}','${dueBy}');`;
+
+    pool.query(queryText).then((result) => {
+    res.sendStatus(200);
+    }).catch((error) => {
+     console.log(error);
+     res.sendStatus(500);
+    })
+});
+
+
+
+
 // get request to get data from our database
 router.get('/' , (req, res) => {
     console.log('in get request!!' , req.body);
