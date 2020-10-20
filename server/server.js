fcabfa28cbde7db00b5e8232b95be175d3465166
Matthew Harris <matthewharrisak@gmail.com>
2020-10-20 08:33:44 -0500

    added heroku info

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
